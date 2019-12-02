5f03fa10edd526499ee036428e2f260589cac030
Lzu Tao <taolzu@gmail.com>
2019-12-02 20:33:50 +0700

    improve ci caching

--- build.bash
+++ build.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-(cd blacksmith && cargo build)
+set -e
+set -u
+set -o pipefail
 
-./blacksmith/target/debug/mdbook-blacksmith "$@"
+CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"$PWD"/target}
+export CARGO_TARGET_DIR
+
+cargo run --locked --manifest-path=blacksmith/Cargo.toml -- "$@"
